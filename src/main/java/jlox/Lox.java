4cb7eb53813f70228cefa14931cf751145f78604
Caique Rodrigues <caiquepeixoto1@gmail.com>
2019-04-01 19:58:49 -0300

    [Chapter 6] Add Parser class.

--- src/main/java/jlox/Lox.java
+++ src/main/java/jlox/Lox.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class Lox {
 
-    private static Boolean hadError;
+    private static Boolean hadError = Boolean.FALSE;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -47,9 +47,12 @@ public class Lox {
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
 
-        for (Token token : tokens) {
-            System.out.println(token);
-        }
+        Parser parser = new Parser(tokens);
+        Expr expression = parser.parse();
+
+        if (hadError) return;
+
+        System.out.println(new AstPrinter().print(expression));
     }
 
     static void error(int line, String message) {
@@ -60,6 +63,14 @@ public class Lox {
         System.err.println("[line " + line + "] Error" + where + ": " + message);
 
         hadError = true;
+    }
+
+    static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
     }
 
 }
